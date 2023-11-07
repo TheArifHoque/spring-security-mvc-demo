c30cfc50a0cfe8a145f174d103f28d27710f664d
TheArifHoque <97823073+TheArifHoque@users.noreply.github.com>
2023-11-07 20:34:17 +0600

    login & logout config, role config

--- src/main/java/com/arifhoque/main/config/WebSecurityConfig.java
+++ src/main/java/com/arifhoque/main/config/WebSecurityConfig.java
@@ -3,6 +3,11 @@ package com.arifhoque.main.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.*;
@@ -11,10 +16,30 @@ import static org.springframework.security.web.util.matcher.AntPathRequestMatche
 public class WebSecurityConfig {
 
     @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user1 = User.withUsername("Arif")
+                .password("1234")
+                .roles("USER")
+                .build();
+        UserDetails user2 = User.withUsername("Hoque")
+                .password("abcd")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user1,user2);
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.
                 authorizeHttpRequests(authConfig -> authConfig
                         .requestMatchers("/home").permitAll()
+                        .requestMatchers("/admin").hasRole("ADMIN")
+                        .requestMatchers("/user").hasRole("USER")
                         .anyRequest().authenticated()
                 )
                 .formLogin(loginConfig -> loginConfig
@@ -22,7 +47,11 @@ public class WebSecurityConfig {
                         .usernameParameter("username")
                         .passwordParameter("password")
                         .loginProcessingUrl("/login")
-                        .successForwardUrl("/user")
+                        .successForwardUrl("/dashboard")
+                        .failureUrl("/home?warning=true")
+                )
+                .logout(logoutConfig -> logoutConfig
+                        .logoutUrl("/logout")
                 )
                 .build();
     }
--- src/main/java/com/arifhoque/main/controller/MainController.java
+++ src/main/java/com/arifhoque/main/controller/MainController.java
@@ -1,22 +1,32 @@
 package com.arifhoque.main.controller;
 
 import jakarta.annotation.Nullable;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class MainController {
 
     @GetMapping("/home")
-    public String homePage(@RequestParam @Nullable String isloginfailed) {
+    public String homePage(@RequestParam @Nullable String warning, Model model) {
+        if (warning != null)
+            model.addAttribute("warning","Incorrect username or password!");
         return "home-page.html";
     }
 
-    @RequestMapping("/user")
+    @PostMapping("/dashboard")
+    public String dashboard(HttpServletRequest request) {
+        if (request.isUserInRole("ADMIN"))
+            return "redirect:/admin";
+        else
+            return "redirect:/user";
+    }
+
+    @GetMapping("/user")
     public String userPage() {
         return "user-page.html";
     }
