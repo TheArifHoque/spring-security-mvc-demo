66bcb95bb4ccc328e12b6a6d25429414b156c1bd
TheArifHoque <97823073+TheArifHoque@users.noreply.github.com>
2023-11-09 20:31:58 +0600

    updated repositories

--- src/main/java/com/arifhoque/main/repository/AuthorityRepo.java
+++ src/main/java/com/arifhoque/main/repository/AuthorityRepo.java
@@ -1,10 +1,10 @@
 package com.arifhoque.main.repository;
 
+import com.arifhoque.main.model.Authority;
 import com.arifhoque.main.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepo extends JpaRepository<User,Integer> {
-    User findUserByEmail(String email);
+public interface AuthorityRepo extends JpaRepository<Authority,Integer> {
 }
